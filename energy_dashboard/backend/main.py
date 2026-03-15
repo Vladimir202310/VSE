ab20f348ad796fbf0004f113c583d3689b85b8b3
Vladimir202310 <148724087+Vladimir202310@users.noreply.github.com>
2026-03-16 01:33:13 +0300

    Update main.py

--- energy_dashboard/backend/main.py
+++ energy_dashboard/backend/main.py
@@ -1,17 +1,5 @@
-import os
 from fastapi import FastAPI
-from .routers import router as records_router  # относительный импорт
+from backend.routers import router as records_router
 
 app = FastAPI()
 app.include_router(records_router, prefix="/records", tags=["records"])
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(
-        "backend.main:app",
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
-    )
-
