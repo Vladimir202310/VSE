7e8766106a27b482e882973ffa05559cbcf77390
Vladimir202310 <148724087+Vladimir202310@users.noreply.github.com>
2026-03-16 11:35:46 +0300

    Update main.py

--- energy_dashboard/backend/main.py
+++ energy_dashboard/backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import router as records_router
+from .routers import router as records_router  # было: from backend.routers
 
 app = FastAPI()
 app.include_router(records_router, prefix="/records", tags=["records"])
